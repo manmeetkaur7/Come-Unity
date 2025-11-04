354dbbc0316beb8074439a13ddb7efb05d04026d
Aebo <arict21@gmail.com>
2025-11-03 16:58:27 -0800

    fixes test page

--- mern-stack/frontend/src/App.jsx
+++ mern-stack/frontend/src/App.jsx
@@ -5,8 +5,6 @@ export default function App() {
   return (
     <Box textAlign="center" p={10}>
       <Heading mb={4}>Come-Unity Frontend is Working 🎉</Heading>
-      <p>Shit took hella long 😭😭😭</p>
-      <br></br>
       <Button colorScheme="teal">Test Button</Button>
     </Box>
   )
